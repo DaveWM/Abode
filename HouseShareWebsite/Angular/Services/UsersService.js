a113dc9cd2a230b286fa9cc66344793c959919cb
DaveWM <dwmartin41@gmail.com>
2014-08-31 14:13:06 +0100

    re-added user service

---
+++ HouseShareWebsite/Angular/Services/UsersService.js
@@ -0,0 +1,16 @@
+﻿angular.module('Services.Users', [])
+.factory('usersService', function($http) {
+    return {
+        getUser: function(id) {
+            return $http.get(server.endpoints.user.getuser.uri, { params: { userId: id } });
+        },
+
+        getHousemates: function(houseId) {
+            return $http.get(server.endpoints.user.gethousemates.uri, { params: { houseId: houseId } });
+        },
+
+        updateUser: function(user) {
+            return $http.post(server.endpoints.user.updateuser.uri, user);
+        }
+    };
+})
